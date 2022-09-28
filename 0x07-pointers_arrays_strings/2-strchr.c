5248580db1143dae4a4262813fa78261983b532b
Mahafsie <mahafsie@gmail.com>
2022-09-28 06:10:29 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,10 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- * _strchr - locates character in string
- * @s: string to locate char
- * @c: character to find
- * Return: character value
+ *  _strchr - locates character in string
+ *  @s: string to locate char
+ *  @c: character to find
+ *  Return: character value
  */
 
 char *_strchr(char *s, char c)
